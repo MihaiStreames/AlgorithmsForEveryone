258099c4d8520d7b5a188252ec95c3d2f25f76a7
Mihai <rares.bors@ulb.be>
2025-03-06 16:46:13 +0100

    deleted old

--- Python/Sorts/Selection_Sort.py
+++
@@ -1,9 +0,0 @@
-def selection_sort(array):
-    for last in range(len(array) - 1, 0, -1):
-        max_pos = 0
-
-        for location in range(1, last + 1):
-            if array[location] > array[max_pos]:
-                max_pos = location
-
-        array[max_pos], array[last] = array[last], array[max_pos]
